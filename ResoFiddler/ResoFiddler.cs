8e0f4d5571ed70936879d2a381ecb9088f108f5f
Christos <christos342@gmail.com>
2024-08-17 10:17:13 +0300

    Added nullcheck.

--- ResoFiddler/ResoFiddler.cs
+++ ResoFiddler/ResoFiddler.cs
@@ -28,7 +28,7 @@ namespace R_Fiddler
 		private static readonly ModConfigurationKey<bool> ENABLED = new("Enabled", "Toggle notifications for external asset loading.", () => true);
 
 		[AutoRegisterConfigKey]
-		private static readonly ModConfigurationKey<string> TRUSTEDURI = new("TrustedURI", "List of trusted domains that won't trigger notifications.", () => "google.com, imgur.com, reddit.com, youtube.com, facebook.com, twitter.com, wikipedia.org, wikimedia.org, discordapp.net, resonite.com");
+		private static readonly ModConfigurationKey<string> TRUSTEDURI = new("TrustedURI", "List of trusted domains that won't trigger notifications.", () => "google.com, imgur.com, reddit.com, youtube.com, facebook.com, twitter.com, wikipedia.org, wikimedia.org, discordapp.net, discordapp.com, resonite.com");
 
 		[AutoRegisterConfigKey]
 		private static readonly ModConfigurationKey<int> COOLDOWN = new("Cooldown", "Set a cooldown period (in seconds) between notifications for the same domain. 0 to disable", () => 5);
@@ -148,7 +148,7 @@ namespace R_Fiddler
 
 				if (currentWorld != null && !currentWorld.IsUserspace() && currentWorld.Name.ToLower() != "local")
 				{
-					worldThumbnail = new(currentWorld?.GenerateSessionInfo()?.ThumbnailUrl);
+					worldThumbnail = new(currentWorld?.GenerateSessionInfo()?.ThumbnailUrl ?? config.GetValue(PLACEHOLDERURI).ToString());
 				}
 
 				if (await Helpers.IsValidImageUrl(target))
